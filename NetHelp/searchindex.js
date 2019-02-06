a2fa48d0ce526d592fd241821abd0f2fe08470ee
Ielich Balababa <iBalababa@madcapsoftware.com>
2019-02-06 11:22:13 -0800

    Initial commit

---
+++ NetHelp/searchindex.js
@@ -0,0 +1,7 @@
+{
+"topics": [["Documents/chapter1.htm","Chapter 1"],["Documents/topic1.htm","Topic 1"],["Documents/chapter11.htm","Chapter 1"],["Documents/topic11.htm","Topic 1"],["Documents/glossaryofterms.htm","Glossary of Terms"],["Documents/gloss_term1.htm","Term1"],["Documents/gloss_term2.htm","Term2"]],
+"words": ["1",":","chapter","definition","glossary","here","insert","term1","term2","terms","text","topic","type","variable","your"],
+"wordTopics": [[0,2,1,8,1,1,1,2,2,1,8,3,1,1],[0,1,6,2,1,6],[0,1,0,2,1,0],[5,1,2,6,1,2],[4,1,0],[0,1,5,1,1,5,2,1,5,3,1,5,5,1,3,6,1,3],[0,1,2,1,1,2,2,1,2,3,1,2],[4,1,2,5,1,0],[4,1,3,6,1,0],[4,1,1],[0,1,4,1,1,4,2,1,4,3,4,4,7,9,11],[0,1,7,1,1,0,2,1,7,3,1,0],[5,1,1,6,1,1],[3,3,6,8,10],[0,1,3,1,1,3,2,1,3,3,1,3]],
+"stopWords": ["a","all","am","an","and","any","are","as","at","be","but","by","can","could","did","do","does","etc","for","from","goes","got","had","has","have","he","her","him","his","how","if","in","is","it","let","me","more","much","must","my","nor","not","now","of","off","on","or","our","own","see","set","shall","she","should","so","some","than","that","the","them","then","there","these","this","those","though","to","too","us","was","way","we","what","when","where","which","who","why","will","would","yes","yet","you"],
+"aliases": []
+}
---
+++ NetHelp/toc.js
@@ -0,0 +1,32 @@
+[
+    {
+        "url": "Documents/chapter1.htm",
+        "title": "Chapter 1",
+        "tooltip": "Chapter 1",
+        "items": [
+            {
+                "url": "Documents/topic1.htm",
+                "title": "Topic 1",
+                "tooltip": "Topic 1"
+            }
+        ]
+    },
+    {
+        "url": "Documents/chapter11.htm",
+        "title": "Chapter 1",
+        "tooltip": "Chapter 1",
+        "items": [
+            {
+                "url": "Documents/topic11.htm",
+                "title": "Topic 1",
+                "tooltip": "Topic 1"
+            }
+        ]
+    },
+    {
+        "url": "Documents/glossaryofterms.htm",
+        "glossary": "1",
+        "title": "Glossary of Terms",
+        "tooltip": "Glossary of Terms"
+    }
+]
